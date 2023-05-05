cdf62a0742dffb0c7c3545802a9ff39d4b620123
Ziilcher <kla.passakorn129@gmail.com>
2023-05-06 01:55:07 +0700

    Edit hompage.

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,19 @@
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
+import './app.css'
+
 import Homepage from "./pages/Homepage"
+import Aboutpage from './pages/Aboutpage'
 import Navbar from "./components/Navbar"
 
 function App() {
   return (
-    <>
-      <Navbar />
-      <Homepage />
-    </>
+    <BrowserRouter>
+      <Navbar/>
+      <Routes>
+        <Route path='home' element={<Homepage/>}/>
+        <Route path='About' element={<Aboutpage/>}/>
+      </Routes>
+    </BrowserRouter>
   )
 }
 
