a7a1f7ba5394d73bcbf52e7231e74f7684c1024e
Ziilcher <kla.passakorn129@gmail.com>
2023-05-06 16:41:02 +0700

    Add the personal data.

---
+++ src/data.jsx
@@ -0,0 +1,63 @@
+export const personalInfo = [
+    {
+        id: 1,
+        title: 'First Name : <br/>',
+        description: 'Passakorn',
+    },
+
+    {
+        id: 2,
+        title: 'Last Name : <br/>',
+        description: 'Poomsophonmetha',
+    },
+
+    {
+        id: 3,
+        title: 'Age : <br/>',
+        description: '25 years',
+    },
+
+    {
+        id: 4,
+        title: 'Gender : <br/>',
+        description: 'Male',
+    },
+
+    {
+        id: 5,
+        title: 'Nationality : <br/>',
+        description: 'Thai',
+    },
+
+    {
+        id: 6,
+        title: 'Address : <br/>',
+        description: 'Bangson, Bangkok',
+    },
+
+    {
+        id: 7,
+        title: 'Phone : <br/>',
+        description: '095-452-5462',
+    },
+
+    {
+        id: 8,
+        title: 'Email : <br/>',
+        description: 'kla.passakorn@hotmail.com',
+    },
+];
+
+export const stats = [
+    {
+        id: 1,
+        no: '1+',
+        title: 'years of Software Engineer <br /> Experience',
+    },
+
+    {
+        id: 2,
+        no: '5+',
+        title: 'Completed <br /> Projects',
+    },
+]
