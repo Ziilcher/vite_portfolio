7974a7e39304b39bb2a9ff25e55991bc5b0fdb90
Ziilcher <kla.passakorn129@gmail.com>
2023-05-06 16:43:59 +0700

    Add about page.

--- src/pages/Aboutpage.jsx
+++ src/pages/Aboutpage.jsx
@@ -1,8 +1,37 @@
-import React from 'react'
+import React, { useEffect } from 'react'
+import Info from '../components/Info'
+import Stats from '../components/Stats'
+import './Aboutpage.css'
+import AOS from 'aos'
 
 function Aboutpage() {
+  useEffect(()=>{
+    AOS.init({duration:500})
+  },[])
+
   return (
-    <div>Aboutpage</div>
+    <main className="section container" data-aos='fade'>
+      <section className="about">
+        <h2 className="section-title">
+          About <span>Me</span>
+        </h2>
+
+        <div className="about-container grid">
+          <div className="about-info">
+            <h3 className="section-subtitle">Personal Infos</h3>
+
+            <ul className='info-list grid'>
+              <Info />
+            </ul>
+          </div>
+
+          <div className="stats grid">
+            <Stats />
+
+          </div>
+        </div>
+      </section>
+    </main>
   )
 }
 
