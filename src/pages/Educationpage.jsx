1ebb99f9f9a06bc683b5d8381c814c1e25959557
Ziilcher <kla.passakorn129@gmail.com>
2023-05-07 20:22:37 +0700

    Add Education.

---
+++ src/pages/Educationpage.jsx
@@ -0,0 +1,35 @@
+import React, { useEffect } from 'react'
+import './Educationpage.css'
+import AOS from 'aos'
+import EduTimline from '../components/EduTimline'
+
+function Educationpage() {
+  useEffect(()=>{
+    AOS.init({duration:500})
+  },[])
+
+  return (
+    <main className="section container" data-aos='fade'>
+        <section className="education">
+            <h2 className="section-title">
+                Education
+            </h2>
+
+            <div className="tab grid">
+                <button className='tablinks'>Education</button>
+                <button className='tablinks'>Experience</button>
+            </div>
+
+            <div className="tabcontent">
+                <EduTimline/>
+            </div>
+
+            {/* <div className="tabcontent">
+                <ExpTimeline/>
+            </div> */}
+        </section>
+    </main>
+  )
+}
+
+export default Educationpage
