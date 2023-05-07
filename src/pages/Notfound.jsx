895fac2f4daa471041cc07b2af379a6f90ce728b
Ziilcher <kla.passakorn129@gmail.com>
2023-05-07 20:26:02 +0700

    Create 404 page.

---
+++ src/pages/Notfound.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+import './Notfound.css'
+
+function Notfound() {
+  return (
+    <main className="section container">
+        <section className="notfound">
+            <h1>404</h1>
+            <h2>Page not found.</h2>
+        </section>
+    </main>
+  )
+}
+
+export default Notfound
