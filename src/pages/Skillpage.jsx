286cafb53c40b3d3d3b2cda07d24dfa080fa253c
Ziilcher <kla.passakorn129@gmail.com>
2023-05-07 20:22:12 +0700

    Add skill page.

---
+++ src/pages/Skillpage.jsx
@@ -0,0 +1,21 @@
+import React, { useEffect } from 'react'
+import AOS from 'aos'
+
+function Skillpage() {
+  
+  useEffect(()=>{
+    AOS.init({duration:500})
+  },[])
+
+  return (
+    <main className="section container" data-aos='fade'>
+        <section className="Skill">
+            <h2 className="section-title">
+                Skill
+            </h2>
+        </section>
+    </main>
+  )
+}
+
+export default Skillpage
