52521796f3b5d019f0a883c1c10698bfad0d7955
Ziilcher <kla.passakorn129@gmail.com>
2023-05-14 02:41:18 +0700

    Update education timeline page.

--- src/components/EduTimeline.jsx
+++ src/components/EduTimeline.jsx
@@ -3,11 +3,13 @@ import { education } from '../data'
 const EduTimeline = () => {
   return (
     <>
-    {education.map(({year,school,index})=>{
+    {education.map(({year,school}, index)=>{
       return (
         <li className="edu-item" key={index}>
-          <span className='year'>{year}<br/></span>
-          <span className='place'>{school}</span>
+          <div className="content">
+            <span className='year'>{year}<br/></span>
+            <span className='place'>{school}</span>
+          </div>
         </li>
       )
     })}
