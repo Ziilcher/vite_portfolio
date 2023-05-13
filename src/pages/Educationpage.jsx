245c3410f41b02960b07b3d4eed7b0f4564c4103
Ziilcher <kla.passakorn129@gmail.com>
2023-05-14 02:43:38 +0700

    Add Expirence content.

--- src/pages/Educationpage.jsx
+++ src/pages/Educationpage.jsx
@@ -1,9 +1,23 @@
-import React, { useEffect } from 'react'
+import { useEffect, useState } from 'react'
 import './Educationpage.css'
 import AOS from 'aos'
 import EduTimeline from '../components/EduTimeline'
+import ExpTimeline from '../components/ExpTimeline'
 
 function Educationpage() {
+  const [Edustate, setEdustate] = useState(true)
+  const [Expustate, setExpustate] = useState(false)
+
+  const OnClickEdu = ()=>{
+    setEdustate(true)
+    setExpustate(false)
+  }
+
+  const OnClickExp = ()=>{
+    setExpustate(true)
+    setEdustate(false)
+  }
+
   useEffect(()=>{
     AOS.init({duration:500})
   },[])
@@ -12,21 +26,24 @@ function Educationpage() {
     <main className="section container" data-aos='fade'>
         <section className="education">
             <h2 className="section-title">
-                Education
+                <pink>Education</pink> and <span>Experience</span> 
             </h2>
 
-            <div className="tab grid">
-                <button className='tablinks'>Education</button>
-                <button className='tablinks'>Experience</button>
+            <div className="tab-wrapper">
+              <div className="tab grid">
+                  <button className='tablinks' onClick={OnClickEdu}>Education</button>
+                  <button className='tablinks' onClick={OnClickExp}>Experience</button>
+              </div>
             </div>
 
-            <div className="tabcontent">
+            <div className={Edustate ? "tabcontent left-content" : "tabcontent left-content hide"}>
                 <EduTimeline/>
             </div>
 
-            {/* <div className="tabcontent">
+            <div className={Expustate ? "tabcontent right-content" : "tabcontent right-content hide"}>
                 <ExpTimeline/>
-            </div> */}
+            </div>
+            
         </section>
     </main>
   )
