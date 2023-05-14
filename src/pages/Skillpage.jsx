56d9ab331e4cb0e69d4341f5225baad9be6886aa
Ziilcher <kla.passakorn129@gmail.com>
2023-05-14 15:02:19 +0700

    Add Skill content.

--- src/pages/Skillpage.jsx
+++ src/pages/Skillpage.jsx
@@ -1,5 +1,7 @@
 import { useEffect } from 'react'
 import AOS from 'aos'
+import SkillFront from '../components/SkillFront'
+import './Skillpage.css'
 
 function Skillpage() {
   
@@ -13,6 +15,28 @@ function Skillpage() {
             <h2 className="section-title">
                 Skill
             </h2>
+
+            <div className="skill-center">
+
+              <div className="skill-container grid">
+
+                <div className="skill-card">
+                  <h3 className="section-subtitle">Frontend developer</h3>
+                  <div className="skill-wrapper grid">
+                    <SkillFront/>
+                  </div>
+                </div>
+
+                {/* <div className="skill-card">
+                  <h3 className="section-subtitle">Backend developer</h3>
+                  <div className="skill-wrapper grid">
+                    <SkillFront/>
+                  </div>
+                </div> */}
+
+
+              </div>
+            </div>
         </section>
     </main>
   )
