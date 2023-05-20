b510afe0bc8a053b6514191953bfe049c2fd3ed3
Ziilcher <kla.passakorn129@gmail.com>
2023-05-20 19:26:42 +0700

    Edit Contact page.

--- src/pages/Contactpage.jsx
+++ src/pages/Contactpage.jsx
@@ -12,15 +12,34 @@ function Contactpage() {
             <h2 className="section-title">Contact</h2>
 
             <div className="contact-container grid">
-                <div className="left-contact">
-                    <h2 className="section-subtitle">Let's work together.</h2>
-                    <input type="text" class='field' placeholder="Your name"/>
-                    <input type="email" class='field' placeholder="Your email"/>
-                    <input type="tel" class='field' placeholder="Your phone number"/>
-                    <textarea cols="30" rows="10" placeholder="Message"/>
-                </div>
-                <div className="right-container">
+                <form className="left-contact" action="">
+                    <h2 className="section-subtitle">Let's work <span>together.</span></h2>
                     
+                    <div className="input-box">
+                      <input type='text' className="input" required/>
+                      <label for=''>Name</label>
+                    </div>
+
+                    <div className="input-box">
+                      <input type="email" className="input" required/>
+                      <label for="">Email</label>
+                    </div>
+
+                    <div className="input-box">
+                      <input type="tel" className="input" required/>
+                      <label for="">Phone</label>
+                    </div>
+
+                    <div className="input-box">
+                      <textarea name="" className="input" required id="message" cols="30" rows="10"></textarea>
+                      <label for="">Message</label>
+                    </div>
+
+                    <input className="submit-btn" type="submit" value="SUBMIT"/>
+                </form>
+                
+                <div className="right-contact">
+                    <div>image</div>
                 </div>                
             </div>
         </section>
